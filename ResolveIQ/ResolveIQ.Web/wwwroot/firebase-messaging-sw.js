03f5362cd02ee8230611616d68d725607d0c6683
Timileyin Ayegbayo <tayegbayo@axxess.com>
2025-09-25 20:00:15 +0100

    added notification listening

---
+++ ResolveIQ/ResolveIQ.Web/wwwroot/firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+﻿importScripts('https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.5.0/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: "YOUR_SENDER_ID"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+    self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body,
+        icon: '/icon.png'
+    });
+});
